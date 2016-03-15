782e2aab28f5d9e14caabe1284b5f2d124d6d795
gotop44 <gotop4455@gmail.com>
2016-03-15 23:21:18 +0800

    Update StillCalculator.java
    Typesetting

--- StillCalculator.java
+++ StillCalculator.java
@@ -16,25 +16,20 @@ public class StillCalculator extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Still Calculator");
-
-        initRootLayout();
+	initRootLayout();
     }
-	
-	
+
     public void initRootLayout() {
         try {
-            
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(StillCalculator.class.getResource("calculate.fxml"));
-            rootLayout = (AnchorPane) loader.load();
-
-            
-            Scene scene = new Scene(rootLayout);
-            primaryStage.setScene(scene);
-            primaryStage.show();
+        	FXMLLoader loader = new FXMLLoader();
+            	loader.setLocation(StillCalculator.class.getResource("calculate.fxml"));
+            	rootLayout = (AnchorPane) loader.load();
+            	Scene scene = new Scene(rootLayout);
+            	primaryStage.setScene(scene);
+            	primaryStage.show();
         }
-		catch (IOException e) {
-            e.printStackTrace();
+	catch (IOException e) {
+            	e.printStackTrace();
         }
     }
 
