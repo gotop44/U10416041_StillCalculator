7affba0f1634e0fbd66b1872abf411b1a050e70e
gotop44 <gotop4455@gmail.com>
2016-03-15 23:18:33 +0800

    Added files via upload

---
+++ StillCalculator.java
@@ -0,0 +1,48 @@
+/** U10416041 */
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class StillCalculator extends Application {
+
+    private Stage primaryStage;
+    private AnchorPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Still Calculator");
+
+        initRootLayout();
+    }
+	
+	
+    public void initRootLayout() {
+        try {
+            
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(StillCalculator.class.getResource("calculate.fxml"));
+            rootLayout = (AnchorPane) loader.load();
+
+            
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+		catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
