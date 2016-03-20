d88b420069ef4ec04ff7373f8338b13e9407b7ef
gotop44 <gotop4455@gmail.com>
2016-03-21 00:15:26 +0800

    Update StillCalculator.java
    add comments and typesetting

--- StillCalculator.java
+++ StillCalculator.java
@@ -7,37 +7,42 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+/** Extends the javafx.application.Application  */
 public class StillCalculator extends Application {
 
-    private Stage primaryStage;
-    private AnchorPane rootLayout;
+    	private Stage primaryStage;
+    	private AnchorPane rootLayout;
 
-    @Override
-    public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("Still Calculator");
-	initRootLayout();
-    }
+    	@Override
+    	public void start(Stage primaryStage) {
+    		
+        	this.primaryStage = primaryStage;
+        	this.primaryStage.setTitle("Still Calculator");
+		initRootLayout();
+    	}
 
-    public void initRootLayout() {
-        try {
-        	FXMLLoader loader = new FXMLLoader();
-            	loader.setLocation(StillCalculator.class.getResource("calculate.fxml"));
-            	rootLayout = (AnchorPane) loader.load();
-            	Scene scene = new Scene(rootLayout);
-            	primaryStage.setScene(scene);
-            	primaryStage.show();
-        }
-	catch (IOException e) {
-            	e.printStackTrace();
-        }
-    }
+	/** Initializes the root layout */
+    	public void initRootLayout() {
+        	try {
+        		/** Load calculate.fxml */
+        		FXMLLoader loader = new FXMLLoader();
+            		loader.setLocation(StillCalculator.class.getResource("calculate.fxml"));
+            		rootLayout = (AnchorPane) loader.load();
+            		Scene scene = new Scene(rootLayout);
+            		primaryStage.setScene(scene);
+            		primaryStage.show();
+        	}
+		catch (IOException e) {
+            		e.printStackTrace();
+        	}
+    	}
 
-    public Stage getPrimaryStage() {
-        return primaryStage;
-    }
+	/** Return the stage */
+    	public Stage getPrimaryStage() {
+        	return primaryStage;
+    	}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    	public static void main(String[] args) {
+        	launch(args);
+    	}
 }
